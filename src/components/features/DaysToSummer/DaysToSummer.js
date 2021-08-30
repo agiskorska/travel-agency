b52dc218c9c0bcfd8da9c113bb17b7b37c6c09b1
Aga Skorska <skorskaga@gmail.com>
2021-08-30 19:13:07 +0100

    Add Days to Summer component and tests

--- src/components/features/DaysToSummer/DaysToSummer.js
+++ src/components/features/DaysToSummer/DaysToSummer.js
@@ -3,11 +3,39 @@ import styles from './DaysToSummer.scss';
 
 class DaysToSummer extends React.Component {
 
+  calculateDays() {
+    const dateNow = new Date();
+    const yearNow = dateNow.getFullYear();
+    const monthNow = dateNow.getMonth();
+    let nextSummerYear = yearNow;
+
+    if (monthNow >= 9) {
+      nextSummerYear = yearNow + 1;
+    }
+
+    const summerBegins = new Date(`06/20/${nextSummerYear}`);
+    const summerEnds = new Date(`09/23/${nextSummerYear}`);
+    console.log(dateNow, summerBegins);
+    let textToDisplay = '';
+    let daysLeft;
+    if(dateNow.getTime() == summerBegins.getTime()) {
+      textToDisplay = '1 day left till summer!';
+    } else if (dateNow.getTime() > summerBegins.getTime() && dateNow.getTime() <= summerEnds.getTime()) {
+      textToDisplay = '';
+    } else {
+      daysLeft = Math.ceil((summerBegins.getTime() - dateNow.getTime())/(1000*3600*24));
+      textToDisplay = daysLeft + ' days left till summer!';
+    }
+
+    return textToDisplay;
+  }
+
+
   render() {
     return (
       <div className={styles.component}>
-        <h3 className={styles.h3}>
-          25 days to summer!
+        <h3 className={styles.daysToSummer}>
+          {this.calculateDays()}
         </h3>
       </div>
     );
--- src/components/features/DaysToSummer/DaysToSummer.test.js
+++ src/components/features/DaysToSummer/DaysToSummer.test.js
@@ -2,9 +2,47 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import DaysToSummer from './DaysToSummer';
 
+const select = {
+  text: '.daysToSummer',
+};
+
 describe('Component DaysToSummer', () => {
   it('should render without crashing', () => {
     const component = shallow(<DaysToSummer />);
     expect(component).toBeTruthy();
   });
+});
+
+
+const trueDate = Date;
+const mockDate = customDate => class extends Date {
+  constructor(...args) {
+    if(args.length){
+      super(...args);
+    } else {
+      super(customDate);
+    }
+    return this;
+  }
+  static now(){
+    return (new Date(customDate)).getTime();
+  }
+};
+
+const checkDescriptionAtDate = (date, expectedDescription) => {
+  it(`should show correct at ${date}`, () => {
+    global.Date = mockDate(`${date}`);
+
+    const component = shallow(<DaysToSummer />);
+    const renderedDate = component.find(select.text).text();
+    expect(renderedDate).toEqual(expectedDescription);
+
+    global.Date = trueDate;
+  });
+};
+
+describe('Component HappyHourAd with mocked Date', () => {
+  checkDescriptionAtDate('06/18/2022', '2 days left till summer!');
+  checkDescriptionAtDate('06/20/2022', '1 day left till summer!');
+
 });
